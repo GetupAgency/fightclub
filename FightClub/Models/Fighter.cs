c5add166a44825967896886e39761d50083ccced
Adrien Cerdan <acerdan01@gmail.com>
2017-06-27 14:32:53 +0200

    Explications et commentaires

---
+++ FightClub/Models/Fighter.cs
@@ -0,0 +1,38 @@
+﻿﻿using System;
+
+namespace FightClub
+{
+    public class Fighter
+    {
+		/** 
+		 * Cette classe définie les caractéristiques d'un combattant
+		 * Chaque propriété est déclaré comme suit :
+		 * public (accessible à d'autres classes) string (type de la variable) get (on peut obtenir la valeur) set (on peut mettre à jour la valeur)
+        **/
+
+		//Propriétés de base
+		public string Name { get; set; }
+		public double Strength { get; set; }
+		public double Health { get; set; }
+		public double Armor { get; set; }
+        public double Exp { get; set; }
+        public int CriticalChance { get; set; }
+		public int CriticalDamage { get; set; }
+
+        // Propriétés qui changent à chaque round
+		public bool IsCritical { get; set; }
+		public bool IsMissed { get; set; }
+		public double CurrentStrength { get; set; }
+
+		// On calcule la force critique en augmentant la force par rapport aux dommages critiques
+		public double CriticalStrength { get{
+                return this.Strength * (this.CriticalDamage / 10);
+            } }
+
+        // Le constructeur de notre classe, qui permet d'initialiser des variables, de faire des traitements etc...
+		public Fighter()
+        {
+            
+        }
+    }
+}
--- FightClub/Program.cs
+++ FightClub/Program.cs
@@ -8,78 +8,111 @@ namespace FightClub
         
         static void Main(string[] args)
         {
+            // On déclare et on initialise les combattants
             Fighter fighter1 = new Fighter
+                // Pour voir les propriétés d'un fighter -> clic droit + Atteindre la déclaration
             {
                 Armor = 45,
                 Strength = 45,
                 Exp = 0,
                 Health = 500,
-                CC = 50,
-                CD = 120,
+                CriticalChance = 50,
+                CriticalDamage = 120,
                 Name = "Ragnaru"
             };
-
 			Fighter fighter2 = new Fighter
 			{
 				Armor = 45,
 				Strength = 55,
 				Exp = 0,
 				Health = 500,
-                CC = 50,
-                CD = 150,
+                CriticalChance = 50,
+                CriticalDamage = 150,
 				Name = "Hercule"
 			};
 
-            List<Fighter> fighters = new List<Fighter>();
-			fighters.Add(fighter1);
-			fighters.Add(fighter2);
+            // On initialise une liste de combattants
+            List<Fighter> fighters = new List<Fighter>
+            {
+                // On ajoute les combattants à notre liste
+                fighter1,
+                fighter2
+            };
 
+            // GoFight(fighters) est la méthode qui réalise le combat, elle renvoie le nom du vainqueur
             Console.WriteLine("And the winner is : "+GoFight(fighters));
 		}
 
 
-        private static string GoFight(List<Fighter> fighters){            
+        private static string GoFight(List<Fighter> fighters){
+
+            // Nombre de rounds
             var rounds = 2000;
 			
+            // On boucle pour chaque round
 			for (int i = 0; i < rounds; i++){
 
-				var critical1 = IsCritical(fighters[0].CC);
-				var critical2 = IsCritical(fighters[1].CC);
-				if(critical1){
-                    fighters[0].Strength = fighters[0].Strength * (fighters[0].CD / 10);
-                    Console.WriteLine(fighters[0].Strength.ToString());
+                // On définit si chaque joueur dispose d'un coup critique lors de ce round
+                fighters[0].IsCritical = IsCritical(fighters[0].CriticalChance);
+				fighters[1].IsCritical = IsCritical(fighters[1].CriticalChance);
+
+                // Si le premier combattant fait un coup critique
+				if(fighters[0].IsCritical){
+                    fighters[0].CurrentStrength = fighters[0].Strength * (fighters[0].CriticalDamage / 10);
                 }
-               
+                else{
+                    fighters[0].CurrentStrength = fighters[0].Strength;
+                }
 
-                fighters[0].Health = fighters[0].Health - (fighters[1].Strength - (fighters[1].Strength * (fighters[0].Armor / 100)));
-                fighters[1].Health = fighters[1].Health - (fighters[0].Strength - (fighters[0].Strength * (fighters[1].Armor / 100)));
+				// Si le second combattant fait un coup critique
+				if (fighters[1].IsCritical)
+				{
+					fighters[1].CurrentStrength = fighters[1].Strength * (fighters[1].CriticalDamage / 10);
+				}
+				else
+				{
+					fighters[1].CurrentStrength = fighters[1].Strength;
+				}
 
+				// On calcule l'impact de la frappe sur la santé de l'adversaire :
+                // Santé = Santé - (Force de l'adversaire - (Force de l'adversaire * (L'armure du receveur / 100))
+				fighters[0].Health = fighters[0].Health - (fighters[1].CurrentStrength - (fighters[1].CurrentStrength * (fighters[0].Armor / 100)));
+                fighters[1].Health = fighters[1].Health - (fighters[0].CurrentStrength - (fighters[0].CurrentStrength * (fighters[1].Armor / 100)));
+
+                // Numéro du round
                 Console.WriteLine("****** ROUND "+i+" *******");
-
-                Console.WriteLine("coup critique : "+critical1.ToString());
-				
+                				
                 Console.WriteLine("health fighter 1 : " + fighters[0].Health.ToString());
 				
                 Console.WriteLine("health fighter 2 : " + fighters[1].Health.ToString());
 
-                if (fighters[0].Health <= 0){
-					return fighters[1].Name;
+                // Si une des deux santé est à 0 ou en dessous
+                if (fighters[0].Health <= 0 || fighters[1].Health <= 0){
+
+                    // Si c'est le premier qui a perdu
+                    if(fighters[0].Health < fighters[1].Health){
+                        return fighters[1].Name;
+                    }
+                    // Sinon c'est l'autre
+                    else{
+						return fighters[0].Name;
+					}
 
                 }
-                else if (fighters[1].Health <= 0){
-                    return fighters[0].Name;
-				}
+              
  
 			}
 
             return null;
         }
 
+        // Methode qui renvoie true ou false, selon la critical chance
         private static bool IsCritical(int cc){
 			Random r = new Random();
 			return NextBool(r,cc);
 
 		}
+        // Methode qui tente de rendre le aléatoire plus performant
 		 static bool NextBool(this Random r, int truePercentage = 50)
 		{
 			return r.NextDouble() < truePercentage / 100.0;
